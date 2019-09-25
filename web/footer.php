ece375a1c052497196a72d77cdf5d6b1adaebd48
Aaron Lee <aaron4288@gmail.com>
2019-09-25 12:00:10 -0600

    test on footer

--- web/footer.php
+++ web/footer.php
@@ -1,22 +1,3 @@
-<!doctype html>
-
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-
-        <title>Aaron's Board Game Bonanza</title>
-        <meta name="description" content="Aaron's Board Game Bonanza">
-
-        <link rel="stylesheet" href="css/master.css">
-        <script src="js/master.js"></script>
-
-    </head>
-
-<body>
-
 <footer>
     <p>Aaron Lee CS313</p>
-  </footer>
-
-</body>
-</html>
+</footer>
