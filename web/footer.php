8e83967ff55437d79bcac69521258daeb20558b4
Aaron Lee <aaron4288@gmail.com>
2019-09-24 21:59:45 -0600

    week2test

---
+++ web/footer.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Aaron's Board Game Bonanza</title>
+        <meta name="description" content="Aaron's Board Game Bonanza">
+
+        <link rel="stylesheet" href="css/master.css">
+        <script src="js/master.js"></script>
+
+    </head>
+
+<body>
+
+<footer>
+    <p>Aaron Lee CS313</p>
+  </footer>
+
+</body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,47 @@
-<?php
+<!doctype html>
 
-  phpinfo();
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
 
-?>
+        <title>Aaron's Board Game Bonanza</title>
+        <meta name="description" content="Aaron's Board Game Bonanza">
+
+        <link rel="stylesheet" href="css/master.css">
+        <script src="js/master.js"></script>
+
+    </head>
+
+<body>
+        
+  <header>
+    <h2>Aaron's Board Game Bonanza</h2>
+  </header>
+
+  <section>
+    <nav>
+      <ul>
+        <li><a href="assignments.html">Assignments</a></li>
+      </ul>
+    </nav>
+    
+    <article>
+      <h1>Five Tribes</h1>
+      <img src="images/fiveTribes.jpg" width="35%" alt="Five Tribes">
+      <p>Crossing into the Land of 1001 Nights, your caravan arrives at the fabled Sultanate of Naqala.<br><br>
+      The old sultan just died and control of Naqala is up for grabs!<br><br>    
+      The oracles foretold of strangers who would maneuver the Five Tribes to gain influence over the legendary city-state.<br><br>    
+      Will you fulfill the prophecy? Invoke the old Djinns, move the Tribes into position at the right time and the Sultanate
+      may become yours!</p><br><br>
+
+      <h1>Terraforming Mars</h1>
+      <img class="img-fluid" src="images/tMars.jpg" alt="Terraforming Mars">
+      <p>In the 2400s, mankind begins to terraform the planet Mars. Giant corporations, sponsored by the World Government on Earth, initiate huge projects to raise the temperature, the oxygen level, and the ocean coverage until the environment is habitable. InTerraforming Mars, you play one of those corporations and work together in the terraforming process, but compete for getting victory points that are awarded not only for your contribution to the terraforming, but also for advancing human infrastructure throughout the solar system, and doing other commendable things.</p>
+    </article>
+  </section>
+<?php include 'footer.php';?>
+
+
+
+</body>
+</html>
