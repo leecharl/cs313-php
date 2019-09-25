c3cefac71ca9afe5fa89241c11250440f304a1e8
Aaron Lee <aaron4288@gmail.com>
2019-09-24 22:02:16 -0600

    fix

---
+++ web/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Aaron's Board Game Bonanza</title>
+        <meta name="description" content="Aaron's Board Game Bonanza">
+
+        <link rel="stylesheet" href="css/master.css">
+        <script src="js/master.js"></script>
+
+    </head>
+
+<body>
+
+<header>
+  <h2>Aaron's Board Game Bonanza</h2>
+</header>
+
+
+</body>
+</html>
