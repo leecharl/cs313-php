0d2d3fc109fa4041e1ae667b15e2fb30fd379b95
Aaron Lee <aaron4288@gmail.com>
2019-10-15 12:46:58 -0600

    db ;

--- web/phpproject/insert.php
+++ web/phpproject/insert.php
@@ -11,7 +11,7 @@ $bgg_link =   $_POST['url'];
 // echo "<br><br><br>";
 
 $sql ="INSERT INTO games(title, publisher, published_year, date_added, bgg_link, usersid) 
-        VALUES ('$title', '$publisher', $published_year, now(), '$bgg_link', 1)";
+        VALUES ('$title', '$publisher', $published_year, now(), '$bgg_link', 1);";
 if(mysqli_query($db, $sql)){
     echo "Records added successfully.";
 } else{
