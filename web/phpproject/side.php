3007c84d062445fb473cf6de5a4978700903086d
Aaron Lee <aaron4288@gmail.com>
2019-10-15 12:27:43 -0600

    asdf

--- web/phpproject/side.php
+++ web/phpproject/side.php
@@ -10,29 +10,29 @@ $sesuserID = $_SESSION['userCheck'];
 <div class="fakeimg">
   <?php 
 
-    foreach ($db->query('SELECT title from games usersID = 1 order by title asc') as $row)
+    foreach ($db->query('SELECT title, gameID from games usersID = 1 order by title asc') as $row)
     {
       echo 'title: ' . $row['title'] . " " . $row['gameID'];
       echo '<br/>';
     }
 
-    foreach ($db->query("SELECT * FROM games where usersID = 1 order by title asc") as $row)
-    {
-      // $gameIDloop = $row['gameID'];
-      // $res = "SELECT count(*) as totalPlayed FROM game_played where gameID = $gameIDloop";
-      // $result = $db->query($res);
+    // foreach ($db->query("SELECT * FROM games where usersID = 1 order by title asc") as $row)
+    // {
+    //   // $gameIDloop = $row['gameID'];
+    //   // $res = "SELECT count(*) as totalPlayed FROM game_played where gameID = $gameIDloop";
+    //   // $result = $db->query($res);
 
 
-      echo '<h3>' . "<a href='index.php?gameID=" . $row['gameID'] . "'>" . $row['title'] . "</a></h3>"; 
-      // echo " (" . "<a href='index.php?removeGame=True&gameID=" . $row['gameID'] . "'>" . "-" . "</a>" . ")";  
-      // echo "</h3>";
-      // while($row2 = $result->fetch_array())
-      //   {
-      //     echo "Total Plays: " . $row2['totalPlayed'] . "<br>";
-      //   }
+    //   echo '<h3>' . "<a href='index.php?gameID=" . $row['gameID'] . "'>" . $row['title'] . "</a></h3>"; 
+    //   // echo " (" . "<a href='index.php?removeGame=True&gameID=" . $row['gameID'] . "'>" . "-" . "</a>" . ")";  
+    //   // echo "</h3>";
+    //   // while($row2 = $result->fetch_array())
+    //   //   {
+    //   //     echo "Total Plays: " . $row2['totalPlayed'] . "<br>";
+    //   //   }
 
-      // echo ' BGG link: <a href="'. $row["bgg_link"]. '">Board Game Geek Link</a>';
-      // echo '<br/><br>';
-    }
+    //   // echo ' BGG link: <a href="'. $row["bgg_link"]. '">Board Game Geek Link</a>';
+    //   // echo '<br/><br>';
+    // }
   ?>
 </div><br>
