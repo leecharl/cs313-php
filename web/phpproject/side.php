8bfa56a945ce4a2db11ad7b37bbcb09ed2e01590
Aaron Lee <aaron4288@gmail.com>
2019-10-15 12:38:46 -0600

    cb

--- web/phpproject/side.php
+++ web/phpproject/side.php
@@ -14,20 +14,20 @@ $sesuserID = $_SESSION['userCheck'];
     foreach ($db->query("SELECT * FROM games where usersid = 1 order by title asc") as $row)
     {
        $gameIDloop = $row['gameid'];
-      // $res = "SELECT count(*) as totalPlayed FROM game_played where gameID = $gameIDloop";
-      // $result = $db->query($res);
-      echo $gameIDloop . "<br>";
+      $res = "SELECT count(*) as totalPlayed FROM game_played where gameid = $gameIDloop";
+      $result = $db->query($res);
+      
 
       echo '<h3>' . "<a href='index.php?gameID=" . $gameIDloop . "'>" . $row['title'] . "</a></h3>"; 
-      // echo " (" . "<a href='index.php?removeGame=True&gameID=" . $row['gameID'] . "'>" . "-" . "</a>" . ")";  
-      // echo "</h3>";
-      // while($row2 = $result->fetch_array())
-      //   {
-      //     echo "Total Plays: " . $row2['totalPlayed'] . "<br>";
-      //   }
+      echo " (" . "<a href='index.php?removeGame=True&gameID=" . $gameIDloop. "'>" . "-" . "</a>" . ")";  
+      echo "</h3>";
+      while($row2 = $result->fetch_array())
+        {
+          echo "Total Plays: " . $row2['totalPlayed'] . "<br>";
+        }
 
-      // echo ' BGG link: <a href="'. $row["bgg_link"]. '">Board Game Geek Link</a>';
-      // echo '<br/><br>';
+      echo ' BGG link: <a href="'. $row["bgg_link"]. '">Board Game Geek Link</a>';
+      echo '<br/><br>';
     }
   ?>
 </div><br>
