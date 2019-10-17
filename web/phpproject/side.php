2e5cfe3dc1bfb494616bd85bd372c2608f832065
Aaron Lee <aaron4288@gmail.com>
2019-10-17 12:12:55 -0600

    db

--- web/phpproject/side.php
+++ web/phpproject/side.php
@@ -10,9 +10,9 @@ $sesuserID = $_SESSION['userCheck'];
 <div class="fakeimg">
   <?php 
     $sql = "SELECT gameid, title FROM games where usersid = 1 order by title asc";
-    $result = $db->query($sql);
+    $sideresult = $db->query($sql);
 
-    while ($row = $result->fetch_assoc())
+    while ($row = $sideresult->fetch_assoc())
     {
     //   echo "<h3>". "<a href='index.php?gameID=" . $row['gameid'] . "'>" . $row['title'] . "</a>";
     //   echo " (" . "<a href='deletegame.php?removeGame=True&gameID=" . $row['gameid'] . "'>" . "-" . "</a>" . ")" ;
