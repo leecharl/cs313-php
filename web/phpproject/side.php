26fa230b6c08a5ca8b29cce59c860afb24d456c1
Aaron Lee <aaron4288@gmail.com>
2019-10-17 12:08:31 -0600

    sdf

--- web/phpproject/side.php
+++ web/phpproject/side.php
@@ -11,23 +11,23 @@ $sesuserID = $_SESSION['userCheck'];
   <?php 
 
 
-$sql = "SELECT gameid, title FROM games where usersid = 1 order by title asc";
-$result = $db->query($sql);
+// $sql = "SELECT gameid, title FROM games where usersid = 1 order by title asc";
+// $result = $db->query($sql);
 
-while ($row = $result->fetch_assoc())
-{
-  echo "<h3>". "<a href='index.php?gameID=" . $row['gameid'] . "'>" . $row['title'] . "</a>";
-  echo " (" . "<a href='deletegame.php?removeGame=True&gameID=" . $row['gameid'] . "'>" . "-" . "</a>" . ")" ;
-  echo " (" . "<a href='addplay.php?gameID=" . $row['gameid'] . "'>" . "+" . "</a>" . ")" .  "</h3>";
+// while ($row = $result->fetch_assoc())
+// {
+//   echo "<h3>". "<a href='index.php?gameID=" . $row['gameid'] . "'>" . $row['title'] . "</a>";
+//   echo " (" . "<a href='deletegame.php?removeGame=True&gameID=" . $row['gameid'] . "'>" . "-" . "</a>" . ")" ;
+//   echo " (" . "<a href='addplay.php?gameID=" . $row['gameid'] . "'>" . "+" . "</a>" . ")" .  "</h3>";
 
-  $idrow =  $row['gameid'];
-  $totalquery = "SELECT count(*) as totalPlayed FROM game_played where gameid =  $idrow";
-  $resulttotal = $db->query($totalquery);
-  while ($totalrow = $resulttotal->fetch_assoc())
-  {
-    echo "Total Plays: " . $totalrow['totalPlayed'] . "<br>";
-  }
-}
+//   $idrow =  $row['gameid'];
+//   $totalquery = "SELECT count(*) as totalPlayed FROM game_played where gameid =  $idrow";
+//   $resulttotal = $db->query($totalquery);
+//   while ($totalrow = $resulttotal->fetch_assoc())
+//   {
+//     echo "Total Plays: " . $totalrow['totalPlayed'] . "<br>";
+//   }
+// }
 
 
   ?>
