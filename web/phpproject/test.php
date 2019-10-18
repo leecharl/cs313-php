f0d831651a6933112792442a61934f532d536d85
Aaron Lee <aaron4288@gmail.com>
2019-10-18 12:40:40 -0600

    test

--- web/phpproject/test.php
+++ web/phpproject/test.php
@@ -1,42 +1,44 @@
 <?php
+
+
 function getDb() {
     try
     {
       $dbUrl = getenv('DATABASE_URL');
-    
-      $dbOpts = parse_url($dbUrl);
-    
-      $dbHost = $dbOpts["host"];
-      $dbPort = $dbOpts["port"];
-      $dbUser = $dbOpts["user"];
-      $dbPassword = $dbOpts["pass"];
-      $dbName = ltrim($dbOpts["path"],'/');
-    
-      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex)
-    {
-      echo 'Error!: ' . $ex->getMessage();
-      die();
-    }
-    //Check connection
-    if (!$db) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    echo "Connected successfully";
+  
+        $dbOpts = parse_url($dbUrl);
+  
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+  
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+        return $db;
+      }
+      catch (PDOException $ex)
+      {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+      }
+  
+  
 }
 
-$test = 2;
+$test = $_GET["gameid"];
 $db = getDB();
   $query = $db->prepare('SELECT title from games where gameid = :test');
   $query->execute(array(":test" => $test));
   $rows = $query->fetchALL(PDO::FETCH_ASSOC);
 
-  foreach($rows as $row) {
+  foreach($rows as $row) 
+  {
 
-    echo $row['title'];
+    echo $row["title"];
   }
 
 
