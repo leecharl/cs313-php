307939773e431a6c80f04cf8d4821ea3ae528588
Aaron Lee <aaron4288@gmail.com>
2019-10-22 13:09:10 -0600

    main add img

--- web/phpproject/main.php
+++ web/phpproject/main.php
@@ -1,24 +1,11 @@
-<?php
-$showGames = false;
-if (isset($_GET["gameID"])) {
-    $showGames = True;
-    $gameID = $_GET["gameID"];
-}
-
-?>
-
-
-
-
-
-
 <div class="fakeimg">
+    <h3>Welcome to Your game tracking system</h3>
+    <div>
+        <img src="https://res.cloudinary.com/csicdn/image/upload/c_pad,fl_lossy,h_350,q_auto,w_350/v1/Images/Products/Misc%20Art/Fantasy%20Flight%20Games/full/FFGMC01.jpg"/>
+    </div>
     
 
 
 
 
 </div>
-<br>
-
-
