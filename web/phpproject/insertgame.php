b89a8b7c2aac0f9c64401d889508082be90e2538
Aaron Lee <aaron4288@gmail.com>
2019-10-21 22:58:28 -0600

    date

--- web/phpproject/insertgame.php
+++ web/phpproject/insertgame.php
@@ -7,9 +7,7 @@
     $title = htmlspecialchars($_POST["title"]);
     $publisher = htmlspecialchars($_POST["publisher"]);
     $bgg_link = htmlspecialchars($_POST["url"]);
-    $date = date("Y-m-d");
-    $time = strtotime($date);
-    $newformat = date('Y-m-d',$time);
+    $gamedate = date("Y-m-d");
 
     require('database.php');
     echo $title. "<br>";
@@ -19,10 +17,10 @@
     $db = getDB();
 
     $stmt1 = $db->prepare('INSERT INTO games(title, publisher, date_added, bgg_link, usersid) 
-    VALUES (:title, :publisher, :newformat, :bgg_link, 1;');
+    VALUES (:title, :publisher, :gamedate, :bgg_link, 1;');
     $stmt1->bindValue(':title', $title, PDO::PARAM_STR);
     $stmt1->bindValue(':publisher', $publisher, PDO::PARAM_STR);
-    $stmt->bindValue(':newformat', $newformat, PDO::PARAM_STR);
+    $stmt->bindValue(':gamedate', $gamedate, PDO::PARAM_STR);
     $stmt1->bindValue(':bgg_link', $bgg_link, PDO::PARAM_STR);
 
     $stmt1->execute();
