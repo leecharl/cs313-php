1ae453d061a83f2e893d6cf11bfe7500ce7152cd
Aaron Lee <aaron4288@gmail.com>
2019-10-21 20:13:03 -0600

    dbg

--- web/phpproject/insertplay.php
+++ web/phpproject/insertplay.php
@@ -10,12 +10,16 @@ $game_played_date = $_POST['game_played_date'];
 $time = strtotime($game_played_date);
 $newformat = date('Y-m-d',$time);
 
-require('database.php');
 
-$db = get_db();
 echo "debug stuff <br>";
 echo $newformat . "<br>";
 echo $game_id . "<br>";
+
+
+require('database.php');
+
+$db = get_db();
+
 $stmt = $db->prepare('INSERT INTO game_played(game_played_date, usersid, gameid) VALUES (:newformat, 1, :game_id);');
 $stmt->bindValue(':game_id', $game_id, PDO::PARAM_INT);
 $stmt->bindValue(':newformat', $newformat, PDO::PARAM_STR);
