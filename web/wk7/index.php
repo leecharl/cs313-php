b8925fa4a484099934c59f7fb38c6dc785c47d8f
Aaron Lee <aaron4288@gmail.com>
2019-10-24 12:49:47 -0600

    wk7

---
+++ web/wk7/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Week 7</title>
+  <meta name="description" content="Week 7">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+  <!-- <script src="js/scripts.js"></script> -->
+
+</head>
+
+<body>
+    <div>
+        <form method="post" action="newuser.php">
+            <div class="form-group">
+                <label for="username">Username: </label>
+                <input id="username" class="form-control" type="text" name="username">
+            </div>
+            <div class="form-group">
+                <label for="password">password</label>
+                <input id="password" class="form-control" type="password" name="password">
+            </div>
+        </form>
+    </div>
+
+  
+</body>
+</html>
