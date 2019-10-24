4e1eefe18aaa5ccd16abd44dcd6866776168a4f1
Aaron Lee <aaron4288@gmail.com>
2019-10-24 13:21:49 -0600

    asdf

--- web/wk7/signin.php
+++ web/wk7/signin.php
@@ -1,6 +1,7 @@
 <?php
 var_dump($_POST);
-
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
     $username = htmlspecialchars($_POST["username"]);
     $password = htmlspecialchars($_POST["password"]);
